1e9438a49368b3bfe6c6bb8fe26a745fa38dfcdc
Joseph Draper <joeyddraper@gmail.com>
2019-03-04 21:05:10 -0600

    adding db port

--- futureConcerts/settings.py
+++ futureConcerts/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'USER': os.environ['DATABASE_USER'],
         'PASSWORD': os.environ['DATABASE_PASSWORD'],
         'HOST': '',
-        'PORT': '',
+        'PORT': os.environ['DATABASE_PORT'],
     }
 }
 # 
@@ -128,9 +128,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
