091c6db969522fc28c0c3365f5ecf7d5720548e6
Joseph Draper <joeyddraper@gmail.com>
2019-03-04 21:26:29 -0600

    push3

--- futureConcerts/settings.py
+++ futureConcerts/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': os.environ['DATABASE_NAME'],
         'USER': os.environ['DATABASE_USER'],
         'PASSWORD': os.environ['DATABASE_PASSWORD'],
-        'HOST': '',
+        'HOST': os.environ['DATABASE_HOST'],
         'PORT': os.environ['DATABASE_PORT'],
     }
 }
